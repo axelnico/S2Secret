016886e21e4c2545506fe5297f3713987434b25d
Nicolas Axel Bukovits <8598589+axelnico@users.noreply.github.com>
2025-06-17 01:07:29 -0300

    WIP: implementation of CBOR for some endpoints with requests encrypted

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,13 @@
+use std::ops::DerefMut;
+
+use coset::{CborSerializable, CoseEncrypt0Builder, HeaderBuilder};
 use secrecy::{zeroize::Zeroize, ExposeSecret, ExposeSecretMut, SecretBox};
 use serde::{Deserialize, Serialize};
 use sharks::{Sharks, Share};
 use tauri::State;
 use tokio::sync::Mutex;
 use tauri_plugin_http::reqwest;
-use opaque_ke::{CipherSuite, ClientLogin, ClientLoginFinishParameters, ClientLoginFinishResult, ClientRegistration, ClientRegistrationFinishParameters, CredentialFinalization, CredentialRequest, CredentialResponse, RegistrationResponse};
+use opaque_ke::{CipherSuite, ClientLogin, ClientLoginFinishParameters, ClientLoginFinishResult, ClientRegistration, ClientRegistrationFinishParameters, CredentialFinalization, CredentialRequest, CredentialResponse, RegistrationRequest, RegistrationResponse, RegistrationUpload};
 use rand::rngs::OsRng;
 use argon2::Argon2;
 use tauri::{Builder, Manager};
@@ -15,6 +18,7 @@ use base64::prelude::*;
 use aes_gcm::{
     aead::{Aead, AeadCore, KeyInit}, aes::Aes256, Aes256Gcm, Key, Nonce
 };
+use aes_gcm::aead::generic_array::typenum::U12;
 use uuid::Uuid;
 
 struct DefaultCipherSuite;
@@ -26,21 +30,29 @@ impl CipherSuite for DefaultCipherSuite {
     type Ksf = Argon2<'static>;
 }
 
-struct SessionHeaderMiddleware {
+struct SecureSessionMiddleware {
     /// Shared state holding the current session id, if any.
-    session_id: Option<uuid::Uuid>,
+    session_id: uuid::Uuid,
+    session_key: SecretBox<Vec<u8>>,
 }
 
 #[async_trait::async_trait]
-impl Middleware for SessionHeaderMiddleware {
+impl Middleware for SecureSessionMiddleware {
     async fn handle(
         &self,
         mut req: Request,
         extensions: &mut Extensions,
         next: Next<'_>,
     ) -> MiddlewareResult<Response> {
-        if let Some(session_id) = &self.session_id {
-            req.headers_mut().insert("session-id", session_id.to_string().parse().unwrap());
+        req.headers_mut().insert("session-id", self.session_id.to_string().parse().unwrap());
+        if let Some(request_body) = req.body_mut() {
+            if let Some(request_body_bytes) = request_body.as_bytes() {
+                let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+                let cose_protected_header = HeaderBuilder::new().algorithm(coset::iana::Algorithm::A256GCM).build();
+                let cose_unprotected_header = HeaderBuilder::new().content_type(String::from("application/cose")).iv(nonce.to_vec()).build();
+                let encrypted_body = CoseEncrypt0Builder::new().protected(cose_protected_header).unprotected(cose_unprotected_header).ciphertext(encrypt_with_nonce(self.session_key.expose_secret(), request_body_bytes, nonce).unwrap_or_default()).build();
+                *request_body = reqwest::Body::from(encrypted_body.to_vec().unwrap_or_default());
+            }
         }
         let res = next.run(req, extensions).await;
         res
@@ -60,35 +72,84 @@ struct S2SecretData {
     http_client: reqwest_middleware::ClientWithMiddleware,
 }
 
+#[derive(Serialize,Deserialize)]
+struct LoginInitialRequest {
+    email: String,
+    message: CredentialRequest<DefaultCipherSuite>,
+}
+
+#[derive(Serialize,Deserialize)]
+struct LoginFinalRequest {
+    email: String,
+    message: CredentialFinalization<DefaultCipherSuite>,
+}
+
+#[derive(Serialize,Deserialize)]
+struct UserDataResponse {
+    id_user: Uuid,
+    email: String,
+    name: String,
+    server_key_file: Vec<u8>
+}
+
+#[derive(Serialize,Deserialize)]
+struct NewSecretRequest {
+    title: String,
+    user_name: Option<String>,
+    site: Option<String>,
+    notes: Option<String>,
+    server_share: String,
+}
+
+#[derive(Serialize,Deserialize)]
+struct UserRegistrationRequest {
+    name: String,
+    email: String,
+    message: RegistrationRequest<DefaultCipherSuite>
+}
+
+#[derive(Serialize,Deserialize)]
+struct UserRegistrationFinishResult {
+    name: String,
+    email: String,
+    message: RegistrationUpload<DefaultCipherSuite>
+}
+
 
 #[tauri::command]
 async fn register_user(email: String, name: String, master_password: String) -> Result<String, ()> {
     let mut client_rng = OsRng;
     let client_registration_start_result = ClientRegistration::<DefaultCipherSuite>::start(&mut client_rng, master_password.as_bytes()).unwrap();
     let registration_request_bytes = client_registration_start_result.message;
-
+    let mut buffer = Vec::new();
+    let registration_request = UserRegistrationRequest {
+        name: name.clone(),
+        email: email.clone(),
+        message: registration_request_bytes.clone(),
+    };
+    ciborium::ser::into_writer(&registration_request,&mut buffer).map_err(|_| ())?;
     let http_client = reqwest::Client::new();
     let registration_initial_response = http_client.post("http://localhost:3000/auth/user/register")
-        .json(&serde_json::json!({
-            "name": name,
-            "email": email,
-            "message": registration_request_bytes
-        }))
+        .body(buffer)
         .send()
         .await
         .map_err(|_| ())?;
     if registration_initial_response.status() != 200 {
         return Err(());
     }
-    let registration_initial_response_json: Vec<u8> = registration_initial_response.json().await.map_err(|_| ())?;
-    let client_registration_finish_result = client_registration_start_result.state.finish(&mut client_rng, master_password.as_bytes(), RegistrationResponse::deserialize(&registration_initial_response_json).unwrap(), ClientRegistrationFinishParameters::default()).unwrap();
+    let response_bytes = registration_initial_response.bytes().await.map_err(|_| ())?;
+    let registration_initial_response: Vec<u8> = ciborium::de::from_reader(response_bytes.as_ref()).map_err(|_| ())?;
+    let client_registration_finish_result = client_registration_start_result.state.finish(&mut client_rng, master_password.as_bytes(), RegistrationResponse::deserialize(&registration_initial_response).unwrap(), ClientRegistrationFinishParameters::default()).unwrap();
     let registration_finish_bytes = client_registration_finish_result.message;
+    buffer = Vec::new();
+    let registration_finish_request = UserRegistrationFinishResult {
+        name: name.clone(),
+        email: email.clone(),
+        message: registration_finish_bytes.clone(),
+    };
+    ciborium::ser::into_writer(&registration_finish_request,&mut buffer).map_err(|_| ())?;
     let registration_final_response = http_client.post("http://localhost:3000/auth/user/register-finalize")
-        .json(&serde_json::json!({
-            "name": name,
-            "email": email,
-            "message": registration_finish_bytes
-        }))
+        .body(buffer)
         .send()
         .await
         .map_err(|_| ())?;
@@ -114,10 +175,11 @@ async fn logged_user_data(state: State<'_, Mutex<S2SecretData>>) -> Result<Strin
     if user_data.status() != 200 {
         return Err(());
     }
-    let user_data_json: serde_json::Value = user_data.json().await.map_err(|_| ())?;
-    state.user_id = Some(Uuid::parse_str(user_data_json["id_user"].as_str().unwrap()).unwrap());
-    state.user_name = Some(user_data_json["name"].as_str().unwrap().to_string());
-    state.user_email = Some(user_data_json["email"].as_str().unwrap().to_string());
+    let response_bytes = user_data.bytes().await.map_err(|_| ())?;
+    let user_data_response: UserDataResponse = ciborium::de::from_reader(response_bytes.as_ref()).map_err(|_| ())?;
+    state.user_id = Some(user_data_response.id_user);
+    state.user_name = Some(user_data_response.name);
+    state.user_email = Some(user_data_response.email);
     Ok("Loaded user data".to_string())
 }
 
@@ -147,17 +209,6 @@ async fn logout(state: State<'_, Mutex<S2SecretData>>) -> Result<String, ()> {
     Ok("User logged out successfully".to_string())
 }
 
-#[derive(Serialize,Deserialize)]
-struct LoginInitialRequest {
-    email: String,
-    message: CredentialRequest<DefaultCipherSuite>,
-}
-
-#[derive(Serialize,Deserialize)]
-struct LoginFinalRequest {
-    email: String,
-    message: CredentialFinalization<DefaultCipherSuite>,
-}
 
 #[tauri::command]
 async fn login(state: State<'_, Mutex<S2SecretData>>, email: String, master_password: String) -> Result<String, ()> {
@@ -181,10 +232,10 @@ async fn login(state: State<'_, Mutex<S2SecretData>>, email: String, master_pass
     }
     let temp_session_id = login_initial_response.headers().get("session-id").unwrap().clone();
     let response_bytes = login_initial_response.bytes().await.map_err(|_| ())?;
-    let login_initial_response_json: Vec<u8> = ciborium::de::from_reader(response_bytes.as_ref()).map_err(|_| ())?;
+    let login_initial_response: Vec<u8> = ciborium::de::from_reader(response_bytes.as_ref()).map_err(|_| ())?;
     let client_login_finish_result = client_login_start_result.state.finish(
         master_password.as_bytes(),
-        CredentialResponse::deserialize(&login_initial_response_json).unwrap(),
+        CredentialResponse::deserialize(&login_initial_response).unwrap(),
         ClientLoginFinishParameters::default(),
     ).unwrap();
     buffer = Vec::new();
@@ -209,8 +260,9 @@ async fn login(state: State<'_, Mutex<S2SecretData>>, email: String, master_pass
     state.password_encryption_key = SecretBox::new(Box::new(Some(client_login_finish_result.export_key.to_vec())));
     let http_client = Client::builder().build().unwrap();
     state.http_client = ClientBuilder::new(http_client)
-    .with(SessionHeaderMiddleware {
-        session_id: state.session_id.clone(),
+    .with(SecureSessionMiddleware {
+        session_id: uuid::Uuid::parse_str(session_id.to_str().unwrap()).unwrap(),
+        session_key: SecretBox::new(Box::new(client_login_finish_result.session_key.to_vec()))
     })
     .build();
     Ok("User login successfully".to_string())
@@ -222,6 +274,13 @@ fn encrypt(key: &[u8], plaintext: &[u8]) -> Result<Vec<u8>, ()> {
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
     let ciphertext = cipher.encrypt(&nonce, plaintext.as_ref()).map_err(|_| ())?;
     Ok([nonce.to_vec(), ciphertext].concat())
+}
+
+fn encrypt_with_nonce(key: &[u8], plaintext: &[u8], nonce: Nonce<U12>) -> Result<Vec<u8>, ()> {
+    let key = Key::<Aes256Gcm>::from_slice(&key[..32]);
+    let cipher = Aes256Gcm::new(&key);
+    let ciphertext = cipher.encrypt(&nonce, plaintext.as_ref()).map_err(|_| ())?;
+    Ok(ciphertext)
 }
 
 fn decrypt(key: &[u8], ciphertext: &[u8]) -> Result<Vec<u8>, ()> {
@@ -256,14 +315,17 @@ async fn add_secret(state: State<'_, Mutex<S2SecretData>>, title: String, user_n
     let encrypted_user_name = encrypt(password_key, user_name.as_bytes())?;
     let encrypted_site =  encrypt(password_key, site.as_bytes())?;
     let encrypted_notes =  encrypt(password_key, notes.as_bytes())?;
+    let mut buffer = Vec::new();
+    let new_secret_request = NewSecretRequest {
+        title: BASE64_STANDARD.encode(encrypted_title),
+        user_name: if user_name.is_empty() { None } else { Some(BASE64_STANDARD.encode(encrypted_user_name)) },
+        site: if site.is_empty() { None } else { Some(BASE64_STANDARD.encode(encrypted_site)) },
+        notes: if notes.is_empty() { None } else { Some(BASE64_STANDARD.encode(encrypted_notes)) },
+        server_share: BASE64_STANDARD.encode(Vec::from(&shares[0])),
+    };
+    ciborium::ser::into_writer(&new_secret_request,&mut buffer).map_err(|_| ())?;
     let add_secret_response = state.http_client.post("http://localhost:3000/secrets")
-        .json(&serde_json::json!({
-            "title": BASE64_STANDARD.encode(encrypted_title),
-            "user_name": BASE64_STANDARD.encode(encrypted_user_name),
-            "server_share": BASE64_STANDARD.encode(Vec::from(&shares[0])),
-            "site": BASE64_STANDARD.encode(encrypted_site),
-            "notes": BASE64_STANDARD.encode(encrypted_notes)
-        }))
+        .body(buffer)
         .send()
         .await
         .map_err(|_| ())?;
