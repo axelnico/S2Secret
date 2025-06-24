8c1d237a4f4d2f9078f8e6da4b77c1a73568f660
Nicolas Axel Bukovits <8598589+axelnico@users.noreply.github.com>
2025-06-23 23:32:55 -0300

    WIP: Add S2Secret logo to login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,7 @@
 <div class="container mx-auto">
   <div class="flex place-content-center">
     <div class="w-full max-w-sm p-6 bg-base-100 rounded-lg shadow-lg">
+      <img src="/s2secret-full-logo.svg" alt="S2Secret Logo"/>
       <h2 class="text-2xl font-bold text-center mb-6">Login</h2>
       
       <!-- Form -->
@@ -74,5 +75,3 @@
     </div>
   </div>
 </div>
-
-
