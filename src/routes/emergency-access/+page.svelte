d890df109ce751b6240fb4eececc46ec8ddc0187
Nicolas Axel Bukovits <8598589+axelnico@users.noreply.github.com>
2025-10-13 01:00:41 -0300

    minor change in warning text for emergency secret access page

--- src/routes/emergency-access/+page.svelte
+++ src/routes/emergency-access/+page.svelte
@@ -62,7 +62,7 @@
   <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 22 22">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
   </svg>
-  <span>Warning: Copy all the information below in a safe place if you need to use it later</span>
+  <span>Warning: Copy all the information below in a safe place if you need to use it later.</span>
 </div>
     <h2 class="card-title border-b border-base-300 pb-2">Recovered Secret</h2>
     
