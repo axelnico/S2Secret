90fddbed47aefc557d88c82e73527dfe3dc39655
Nicolas Axel Bukovits <8598589+axelnico@users.noreply.github.com>
2025-11-03 01:00:47 -0300

    allow to show and hide emergency contact password

--- src/components/EmergencyContact.svelte
+++ src/components/EmergencyContact.svelte
@@ -17,8 +17,13 @@
     let password = $state("");
 
     async function revealPassword() {
-      password = await invoke<string>("reveal_password", { secretId: emergencyContact.id_emergency_contact });
+      password = await invoke<string>("reveal_emergency_contact_password", { emergencyContactId: emergencyContact.id_emergency_contact });
       passwordVisible = true;
+    }
+
+    async function hidePassword() {
+      password = "";
+      passwordVisible = false;
     }
 
     async function copyPasswordToClipboard() {
@@ -45,40 +50,49 @@
             </button>
         </div>
         <div class="divider divider-horizontal divider-primary"></div>
-        <div class="card bg-base-300 rounded-box h-20 flex flex-row grow items-center space-x-2 px-4 flex-1">
+         <div class="card bg-base-300 rounded-box h-20 flex flex-row grow items-center space-x-2 px-4 flex-1">
           <span class="font-semibold mr-2">Password:</span>
-      <button class="pointer-events-auto p-2 bg-transparent border-none text-accent hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-success mr-2" onclick={copyPasswordToClipboard}>
-        {#if passwordVisible}
-        <span class="text-base normal-case">{password}</span>
-        {:else}
-        <div class="flex items-center">
-          {#each { length: 12 }, password}
-          <svg width="15" height="15" viewBox="0 0 48 48" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
-            <path d="M24 33C28.9706 33 33 28.9706 33 24C33 19.0294 28.9706 15 24 15C19.0294 15 15 19.0294 15 24C15 28.9706 19.0294 33 24 33Z" fill="currentColor" stroke="currentColor" stroke-width="4"/>
-            </svg>
-          {/each}
-        </div>
-        {/if}
+      <button aria-label="copy-password" class="pointer-events-auto p-2 bg-transparent border-none text-accent hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-success mr-2" onclick={copyPasswordToClipboard}>
+        <!-- Use bg-transparent to remove input background -->  
+        <input 
+              type={passwordVisible ? 'text' : 'password'}
+              id="password" 
+              name="password"
+              autocorrect="off"
+              autocapitalize="off"
+              autocomplete="off"
+              readonly
+              required
+              maxlength="128"
+              class="border-none w-full input input-secondary join-item font-mono focus:outline-none"
+              value={passwordVisible ? password : "************"}
+            />
       </button>
         </div>
+      {#if passwordVisible}
+              <button class="pointer-events-auto p-2 bg-transparent border-none text-accent hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-success mr-2" onclick={hidePassword}>
+              <svg fill="currentColor" width="25" height="25" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+      <title>Hide password</title>
+      <path d="M28 14v-6.5c0-1.933-1.567-3.5-3.5-3.5s-3.5 1.567-3.5 3.5v5.933c2.697 1.676 4.5 4.657 4.5 8.067 0 5.247-4.253 9.5-9.5 9.5s-9.5-4.253-9.5-9.499c0-5.247 4.253-9.5 9.5-9.5 1.050 0 2.056 0.177 3 0.491v-4.992c0-3.037 2.463-5.5 5.5-5.5s5.5 2.463 5.5 5.5v6.5h-2zM16 13.5c-4.418 0-8 3.582-8 8s3.582 7.999 8 7.999 8-3.581 8-7.999c0-4.418-3.582-8-8-8zM16 29c-4.143 0-7.5-3.357-7.5-7.499 0-4.144 3.357-7.501 7.5-7.501s7.5 3.357 7.5 7.501c0 4.142-3.357 7.499-7.5 7.499zM17.5 19.501c0-0.829-0.672-1.5-1.5-1.5s-1.5 0.671-1.5 1.5c0 0.709 0.504 1.277 1.167 1.432l-1.167 4.567h3.062l-1.314-4.55c0.705-0.123 1.252-0.709 1.252-1.449z"></path>
+      </svg>
+            </button>
+            {:else}
+            <button class="pointer-events-auto p-2 bg-transparent border-none text-accent hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-success mr-2" onclick={revealPassword}>
+              <svg
+                fill="currentColor"
+                width="25"
+                height="25"
+                viewBox="0 0 32 32"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Show password</title>
+                <path d="M16 31c-5.247 0-9.5-4.254-9.5-9.5 0-3.41 1.802-6.391 4.5-8.067v-5.933c0-3.038 2.463-5.5 5.5-5.5s5.5 2.462 5.5 5.5v6.637c2.135 1.742 3.5 4.392 3.5 7.363 0 5.246-4.253 9.5-9.5 9.5zM20 7.5c0-1.933-1.566-3.5-3.5-3.5-1.933 0-3.5 1.567-3.5 3.5v4.991c0.944-0.314 1.95-0.491 3-0.491 1.432 0 2.783 0.325 4 0.892v-5.392zM16 13.5c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM16 29c-4.143 0-7.5-3.357-7.5-7.5s3.357-7.5 7.5-7.5c4.143 0 7.5 3.357 7.5 7.5s-3.357 7.5-7.5 7.5zM17.5 19.5c0-0.828-0.672-1.5-1.5-1.5s-1.5 0.672-1.5 1.5c0 0.711 0.504 1.277 1.167 1.434l-1.167 4.566h3.062l-1.314-4.551c0.705-0.121 1.252-0.709 1.252-1.449z"></path>
+              </svg>
+            </button>
+            {/if}
+        
       </div>
-     
-      
-      
-
-      <button class="pointer-events-auto p-2 bg-transparent border-none text-accent hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-success mr-2" onclick={revealPassword}>
-        <svg
-          fill="currentColor"
-          width="25"
-          height="25"
-          viewBox="0 0 32 32"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <title>{passwordVisible ? "Hide password" : "Show password"}</title>
-          <path d="M16 31c-5.247 0-9.5-4.254-9.5-9.5 0-3.41 1.802-6.391 4.5-8.067v-5.933c0-3.038 2.463-5.5 5.5-5.5s5.5 2.462 5.5 5.5v6.637c2.135 1.742 3.5 4.392 3.5 7.363 0 5.246-4.253 9.5-9.5 9.5zM20 7.5c0-1.933-1.566-3.5-3.5-3.5-1.933 0-3.5 1.567-3.5 3.5v4.991c0.944-0.314 1.95-0.491 3-0.491 1.432 0 2.783 0.325 4 0.892v-5.392zM16 13.5c-4.418 0-8 3.582-8 8s3.582 8 8 8 8-3.582 8-8-3.582-8-8-8zM16 29c-4.143 0-7.5-3.357-7.5-7.5s3.357-7.5 7.5-7.5c4.143 0 7.5 3.357 7.5 7.5s-3.357 7.5-7.5 7.5zM17.5 19.5c0-0.828-0.672-1.5-1.5-1.5s-1.5 0.672-1.5 1.5c0 0.711 0.504 1.277 1.167 1.434l-1.167 4.566h3.062l-1.314-4.551c0.705-0.121 1.252-0.709 1.252-1.449z"></path>
-        </svg>
-      </button>
+    
     </div>
   </div>
 </div>
